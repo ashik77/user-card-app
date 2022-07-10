72d5115afff1b359edcf66936c59bb22c39d1658
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-07-10 19:23:07 +0300

    done except refactoring

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,36 +3,68 @@ import { Row, Card, Button } from "react-bootstrap";
 
 import { useParams } from "react-router";
 import { useNavigate } from "react-router-dom";
-import { axios } from "axios";
 
 function Details() {
   const [detail, setDetail] = useState({});
+  const [address, setAddress] = useState({});
+  const [company, setCompany] = useState({});
+
   let { id } = useParams();
   console.log(id);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const url = `https://jsonplaceholder.typicode.com/users/${id}`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setDetail(data));
+      .then((data) => setDetail(data, data.address));
   }, []);
+
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setAddress(data.address));
+  }, []);
+
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCompany(data.company));
+  }, []);
+
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/home");
+  };
 
   return (
     <Row>
       <Card
-        className="m-3 p-3 mx-auto shadow p-3 mb-5 bg-white rounded"
-        style={{ width: "18rem" }}
-        key={detail.id}
+        className="m-3 p-3 mx-auto shadow p-3 mb-5 bg-white rounded font-weight-bold"
+        style={{ width: "22rem" }}
       >
         <Card.Body>
-          <Card.Title>{detail.name}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            @{detail.username}
-          </Card.Subtitle>
-          <Card.Link href="#">{detail.email}</Card.Link>
-
-          <Button variant="primary">Go Back</Button>
+          <p>-name: {detail.name}</p>
+          <p>-username: {detail.username}</p>
+          <p>-email: {detail.email}</p>
+          <p>-phone: {detail.phone}</p>
+          <p>-company: {company.name}</p>
+          <p>-website: {detail.website}</p>
+          <p>-address:</p>
+          <ul>
+            <li>street: {address.street}</li>
+            <li>suite: {address.suite}</li>
+            <li>city: {address.city}</li>
+            <li>zipcode: {address.zipcode}</li>
+          </ul>
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            onClick={handleClick}
+          >
+            Back
+          </button>
         </Card.Body>
       </Card>
     </Row>
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { Row, Card, Button } from "react-bootstrap";
+import { Row, Card, Button, Container } from "react-bootstrap";
 
 function UserCard() {
   const [users, setUsers] = useState([]);
@@ -19,35 +19,35 @@ function UserCard() {
   }, []);
 
   return (
-    <Row>
-      {users.map((user) => (
-        <Card
-          className="m-3 p-3 mx-auto shadow p-3 mb-5 bg-white rounded"
-          style={{ width: "18rem" }}
-          key={user.id}
-        >
-          <Card.Body>
-            <span class="avatar avatar-96 bg-secondary text-white rounded-circle">
-              {user.name.charAt(0)}
-            </span>
-
-            <Card.Title>{user.name}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              @{user.username}
-            </Card.Subtitle>
-            <Card.Link href="#">{user.email}</Card.Link>
-
-            <Link
-              className="text-decoration-none"
-              style={{ color: "#f06c4e" }}
-              to={`/${user.id}`}
-            >
-              View Detail
-            </Link>
-          </Card.Body>
-        </Card>
-      ))}
-    </Row>
+    <Container>
+      <Row>
+        {users.map((user) => (
+          <Card
+            className="m-3 p-3 mx-auto shadow p-3 mb-5 bg-white rounded text-center"
+            style={{ width: "18rem" }}
+            key={user.id}
+          >
+            <Card.Body>
+              <span className="avatar avatar-96 bg-secondary text-white rounded-circle">
+                {user.name.charAt(0)}
+              </span>
+              <Card.Title className="mt-2">{user.name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                @{user.username}
+              </Card.Subtitle>
+              <Card.Link className="mb-2" href="#">
+                {user.email}
+              </Card.Link>
+              <Link to={`/${user.id}`}>
+                <Button className="mt-3" variant="primary">
+                  MORE DETAILS
+                </Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
